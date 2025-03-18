879f61b6574657b5e6a6af60fb113aaf111ee5a7
jasmunathwal <jasmunathwal@gmail.com>
2025-03-18 15:40:16 +0000

    Updated gemini model to 2.0-flash from 1.5pro

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ const getResponse = async (userPrompt) => {
 
   const genAI = new GoogleGenerativeAI(API_KEY);
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.0-flash",
     safetySettings: [
       {
         category: HarmCategory.HARM_CATEGORY_HARASSMENT,
